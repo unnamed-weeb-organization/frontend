6662fee909fb8c854fa65a904751f84f3e9633e0
Rachala Ovin <resistlyfe@gmail.com>
2022-12-23 12:37:13 +0530

    chore: added a loader to stop FOC from hydration

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,11 +1,16 @@
 <script>
-	import { onMount } from "svelte";
-	import { initializeTheme } from "$lib/hooks/theme";
-	import NavigationBar from "$lib/components/NavigationBar.svelte";
 	import "../app.css";
+	import { onMount } from "svelte";
+	import { fade } from "svelte/transition";
+	import { initializeTheme } from "$lib/hooks/theme";
+	import Spinner from "$lib/components/common/Spinner.svelte";
+	import NavigationBar from "$lib/components/NavigationBar.svelte";
+
+	let loaded = false;
 
 	onMount(() => {
 		initializeTheme();
+		loaded = true;
 	});
 </script>
 
@@ -13,8 +18,18 @@
 	<title>unnamed_weeb_music_database</title>
 </svelte:head>
 
-<div class="relative flex flex-col h-full">
-	<NavigationBar />
-	<div class="escape_navbar" />
-	<slot />
-</div>
+{#if loaded}
+	<div class="relative flex flex-col h-full">
+		<NavigationBar />
+		<div class="escape_navbar" />
+		<slot />
+	</div>
+{:else}
+	<div
+		out:fade={{ duration: 150 }}
+		style="background-color: #1c1c1c"
+		class="flex flex-col h-full justify-center items-center"
+	>
+		<Spinner height="56" width="56" />
+	</div>
+{/if}
