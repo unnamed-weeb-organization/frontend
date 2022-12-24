296be3a2e6eccad3e11370de31887d1f20296210
Rachala Ovin <resistlyfe@gmail.com>
2022-12-24 16:15:01 +0530

    feat: added server models

---
+++ src/lib/typings/server/release.ts
@@ -0,0 +1,12 @@
+export enum ReleaseType {
+	Album = "Album",
+	EP = "EP",
+	Single = "Single",
+}
+
+export interface Release {
+	id: string;
+	name: string;
+	release_type: ReleaseType;
+	total_tracks: number;
+}
