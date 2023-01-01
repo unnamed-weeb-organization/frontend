a14ddf05c17dda4cd9aa159b3105a6373b4174f9
Rachala Ovin <resistlyfe@gmail.com>
2023-01-01 21:20:04 +0530

    fix: cookies being scoped to /auth

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,18 +1,25 @@
+import type { Cookies } from "@sveltejs/kit";
+
 export const APP_NAME = "unnamed_weeb_music_database";
 export const API_URL_DEV = "http://localhost:6001/graphql";
 export const API_URL_PROD = "";
 
 export const errorMessages: Record<number, string> = {
-    401: "Not authorized",
-    404: "Page not found",
-    500: "Internal server error"
+	401: "Not authorized",
+	404: "Page not found",
+	500: "Internal server error"
 };
 
 export const hintMessages: Record<number, string> = {
-    401: "You need to be logged in to access this page.",
-    404: "The page you are looking for might have been removed, had its name changed, or is temporarily unavailable.",
-    500: "Please try again later."
+	401: "You need to be logged in to access this page.",
+	404: "The page you are looking for might have been removed, had its name changed, or is temporarily unavailable.",
+	500: "Please try again later."
 };
 
 export const COOKIE_USER_ID = "user-id";
 export const COOKIE_USER_PAT = "user-pat";
+export const COOKIE_AUTH_OPTIONS: Parameters<Cookies["set"]>[2] = {
+	httpOnly: true,
+	path: "/",
+	sameSite: "strict"
+};
--- src/routes/auth/login/+page.server.ts
+++ src/routes/auth/login/+page.server.ts
@@ -3,23 +3,23 @@ import type { Actions, PageServerLoad } from "./$types";
 import { user } from "$lib/placeholders";
 import { getMissingFields } from "$lib/utils";
 import { Route, RoutePoint } from "$lib/routes";
-import { COOKIE_USER_ID, COOKIE_USER_PAT } from "$lib/constants";
+import { COOKIE_AUTH_OPTIONS, COOKIE_USER_ID, COOKIE_USER_PAT } from "$lib/constants";
 
 export const actions: Actions = {
 	default: async ({ request, cookies }) => {
 		const data = await request.formData();
 		const fields = {
 			username: data.get("username")?.toString(),
-			password: data.get("password")?.toString(),
-		}
+			password: data.get("password")?.toString()
+		};
 
 		const missing = getMissingFields(fields);
 		console.log(missing);
 		if (missing.length > 0) return fail(400, { missing });
-		
-		cookies.set(COOKIE_USER_ID, user.id);
-		cookies.set(COOKIE_USER_PAT, "PersonalAccessToken");
-		
+
+		cookies.set(COOKIE_USER_ID, user.id, COOKIE_AUTH_OPTIONS);
+		cookies.set(COOKIE_USER_PAT, "PersonalAccessToken", COOKIE_AUTH_OPTIONS);
+
 		throw redirect(303, data.get("from")?.toString() ?? Route[RoutePoint.Home].route);
 	}
 };
