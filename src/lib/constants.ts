96b7e27e6c833ae2870a41b6df08d7b477dacf0b
resistlyfe@gmail.com <resistlyfe@gmail.com>
2023-01-09 05:44:46 -0800

    chore: additional error messages and status code responses

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -32,23 +32,29 @@ export const hintMessages: Partial<Record<HTTPCode, string>> = {
 	403: "You do not have permission to access this page.",
 	404: "The page you are looking for might have been removed, had its name changed, or is temporarily unavailable.",
 	422: "The data you provided is invalid.",
-	500: "Please try again later."
+	500: "Something poof'd within the server, please try again later."
 };
 
 export const NOT_VERIFIED_RESOURCE_EDIT = "Only verified users can create/edit resources.";
 export const INVALID_ARTIST_TYPE = "The artist type provided is invalid.";
 export const INVALID_COUNTRY = "The country provided is invalid.";
 export const INVALID_DATE = "The date provided is invalid.";
+export const INVALID_EMAIL = "The email provided is invalid.";
+export const INVALID_PASSWORD_LENGTH = "Password must be at least 8 characters long!";
+export const INVALID_USERNAME_SPECIAL_CHARS = "Username should not contain any special characters.";
 
 export const COOKIE_USER_ID = "user-id";
 export const COOKIE_USER_REFRESH = "user-refresh";
 export const COOKIE_USER_SESSION = "user-session";
-export const COOKIE_AUTH_OPTIONS: Parameters<Cookies["set"]>[2] = {
+export const COOKIE_USER_ISSUED = "user-issued";
+
+export const COOKIE_AUTH_SESSION_OPTIONS: Parameters<Cookies["set"]>[2] = {
 	httpOnly: true,
 	path: "/",
-	sameSite: "strict"
+	sameSite: "strict",
+	expires: new Date(Date.now() + 1000 * 60 * 15)
 };
 export const COOKIE_AUTH_PERSISTENT_OPTIONS: Parameters<Cookies["set"]>[2] =
-	COOKIE_AUTH_OPTIONS && {
+	COOKIE_AUTH_SESSION_OPTIONS && {
 		expires: new Date(Date.now() + 1000 * 60 * 60 * 24 * 30)
 	};
