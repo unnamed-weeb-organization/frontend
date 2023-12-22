5b7008d6d0b571b85c09d641dbc0041ad17b3576
Curstantine <curstantine@gmail.com>
2023-12-22 14:30:18 +0530

    chore: add style resets

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,22 +1,22 @@
 <script lang="ts">
+	import "@unocss/reset/tailwind-compat.css";
 	import "virtual:uno.css";
 	import "../app.css";
 
 	import { onMount } from "svelte";
 
 	import { page } from "$app/stores";
-	import { browser, dev } from "$app/environment";
 	import type { LayoutData } from "./$types";
 
 	import { concatPageTitle } from "$lib/utils";
-	import { APP_NAME, DB_SOURCE, FE_SOURCE } from "$lib/constants";
+	import { APP_NAME } from "$lib/constants";
 	import { initializeSettings, preferredTitleLocale } from "$lib/settings";
 
 	import NavigationBar from "$lib/components/NavigationBar.svelte";
 
 	export let data: LayoutData;
 
-	if (browser) {
+	onMount(() => {
 		const isDark =
 			"is-theme-mode-dark" in localStorage
 				? localStorage.getItem("is-theme-mode-dark") === "true"
@@ -24,9 +24,7 @@
 
 		document.documentElement.classList.add(isDark ? "dark" : "light");
 		document.documentElement.classList.remove("no-transition");
-	}
 
-	onMount(() => {
 		initializeSettings();
 	});
 </script>
@@ -35,19 +33,8 @@
 	<title>{concatPageTitle($page.data.title ?? APP_NAME, $preferredTitleLocale)}</title>
 </svelte:head>
 
-{#if !dev}
-	<div class="flex flex-col items-center justify-center text-center font-head h-full">
-		<h1 class="font-medium text-4xl text-custom-100">Under Maintenance!</h1>
-		<span class="text-custom-200">Will come back online when the backend is live.</span>
-		<div class="flex gap-8 font-mono text-accent-300 mt-8" data-sveltekit-preload-data="off">
-			<a href={FE_SOURCE}>source:frontend</a>
-			<a href={DB_SOURCE}>source:database</a>
-		</div>
-	</div>
-{:else}
-	<div class="relative flex flex-col h-full">
-		<NavigationBar userCardData={data.userCardData ?? null} />
-		<div class="escape_navbar" />
-		<slot />
-	</div>
-{/if}
+<div class="relative flex flex-col h-full">
+	<NavigationBar userCardData={data.userCardData ?? null} />
+	<div class="escape_navbar" />
+	<slot />
+</div>
